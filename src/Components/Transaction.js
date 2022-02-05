1972b97761c1d6dde28d46e0c8bb86e0afbdf08d
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-02-04 22:06:07 -0500

    Rename Transaction Name

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -13,7 +13,7 @@ function Transaction({ transaction, index }) {
       </td>
       <td></td>
       <td>
-        <Link to={`/transactions/${index}`}>{transaction.item_name}</Link>
+        <Link to={`/transactions/${index}`}>{transaction.name}</Link>
       </td>
       <td>
         {transaction.amount ? (
